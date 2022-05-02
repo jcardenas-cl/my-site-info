985f94adc7bf4dfc4d0e6fb920478bee92a2feb6
Julio Cárdenas <julio.cardenas@laika.cl>
2022-05-02 18:10:12 -0400

    Shortcodes para modos de contacto listos

--- includes/msi-frontend.php
+++ includes/msi-frontend.php
@@ -3,11 +3,15 @@
  * Shortcode para mostrar lista con telefonos celulares en el sitio.
  */
 add_shortcode( 'msi_show_mobile_phone_bar', function( $args ) {
+    $phones = explode( ',', get_option( 'msi_mobile_phone' ) );
+    /**
+     * Valores por defecto cuando se llama al shortcode sin parametros, listamos todos los teléfonos ingresados e iniciamos desde el primer registro.
+     */
     $args = shortcode_atts( array (
-        'limit' => 99,
+        'limit' => count($phones),
         'start' => 0,
     ), $args);
-    $phones = explode( ',', get_option( 'msi_mobile_phone' ) );
+    
     $output = '<ul class="msi-mobile-phone-list">'; // String con la lista de telefonos.
     for( $i = $args['start']; $i <= ($args['start'] + $args['limit']) - 1; $i++ ) {
         if ( isset($phones[$i]) ) {
@@ -22,18 +26,64 @@ add_shortcode( 'msi_show_mobile_phone_bar', function( $args ) {
 } );
 
 /**
- * Shortcode para mostrar lista con telefonos celulares en el sitio.
+ * Shortcode para mostrar lista con telefonos en el sitio.
  */
 add_shortcode( 'msi_show_phone_bar', function( $args ) {
+    $phones = explode( ',', get_option( 'msi_phone' ) );
     $args = shortcode_atts( array (
-        'limit' => 99,
+        'limit' => count($phones),
         'start' => 0,
     ), $args);
-    $phones = explode( ',', get_option( 'msi_phone' ) );
+    
     $output = '<ul class="msi-phone-list">'; // String con la lista de telefonos.
-    for( $i = $args['start']; $i <= $args['start'] + $args['count']; $i++ ) {
-        if ( !is_null($phones[$i]) ) {
+    for( $i = $args['start']; $i <= ($args['start'] + $args['limit']) - 1; $i++ ) {
+        if ( isset($phones[$i]) ) {
             $output .= '<li><a href="tel:' . $phones[$i] . '">' . trim($phones[$i]) . '</a></li>';
+        } else {
+            break;
+        }
+    }
+    $output .= '</ul>';
+
+    return $output;
+} );
+
+/**
+ * Shortcode para mostrar lista con correos de contacto en el sitio.
+ */
+add_shortcode( 'msi_show_email_bar', function( $args ) {
+    $emails = explode( ',', get_option( 'msi_email' ) );
+    $args = shortcode_atts( array (
+        'limit' => count($emails),
+        'start' => 0,
+    ), $args);
+    $output = '<ul class="msi-email-list">'; // String con la lista de telefonos.
+    for( $i = $args['start']; $i <= ($args['start'] + $args['limit']) - 1; $i++ ) {
+        if ( isset($emails[$i]) ) {
+            $output .= '<li><a href="mailto:' . $emails[$i] . '">' . trim($emails[$i]) . '</a></li>';
+        } else {
+            break;
+        }
+    }
+    $output .= '</ul>';
+
+    return $output;
+} );
+
+/**
+ * Shortcode para mostrar lista con correos de contacto en el sitio.
+ */
+add_shortcode( 'msi_show_whatsapp_bar', function( $args ) {
+    $phones = explode( ',', get_option( 'msi_whatsapp' ) );
+    $args = shortcode_atts( array (
+        'limit' => count($phones),
+        'start' => 0,
+    ), $args);
+    
+    $output = '<ul class="msi-whatsapp-list">'; // String con la lista de telefonos.
+    for( $i = $args['start']; $i <= ($args['start'] + $args['limit']) - 1; $i++ ) {
+        if ( isset($phones[$i]) ) {
+            $output .= '<li><a href="https://api.whatsapp.com/send/?phone:' . $phones[$i] . '">' . trim($phones[$i]) . '</a></li>';
         } else {
             break;
         }
